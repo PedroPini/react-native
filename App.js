8f76a51fc803f5d2fef238d9e813270820b9c5dd
pedro <pedropini@raptortech.com.au>
2022-02-21 10:17:22 +0800

    Learning Touch, View and Images

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableWithoutFeedback, TouchableOpacity, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("text clicked")
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text >Hello !</Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <Image 
+        fadeDuration={1000}
+        source={{
+          width:200,
+          height:300,
+          uri: "https://picsum.photos/200/300"
+          }} />
+      </TouchableOpacity>
+        <TouchableNativeFeedback>
+          <View style={{ width:200, height:70, backgroundColor:"dodgerblue"}}></View>
+        </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
